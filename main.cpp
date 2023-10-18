9bb266e0ef88e9d01767276df52212aabc6abb58
Nguyen Lam Nghia <nghia.nl214022@sis.hust.edu.vn>
2023-10-18 17:54:31 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickView>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
+
+    QQuickView view;
+    view.engine()->addImportPath("qrc:/UI/imports");
+//    view.setSource(QUrl("qrc:/UI/content/Screen01.ui.qml"));
+    view.setSource(QUrl("qrc:/UI/content/Test.qml"));
+    if (!view.errors().isEmpty())
+        return -1;
+    view.show();
+
+    return app.exec();
+}
